aac5124ee5d63302e88abe151b5f1fc8f269d204
Iain Law <sp1a@rpiXX>
2020-01-10 12:22:26 -0800

    student.c

---
+++ student.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+typedef struct{
+	char fn[100];
+	char ln[100];
+	int age;
+	int id;
+} student;
+
+void printstud(student* stud)
+{
+	printf("Name: %s %s\n", stud->fn, stud->ln);
+	printf("Age: %d\n", stud->age);
+	printf("id: %d\n", stud->id);
+}
+
+void enterstud(student* stu)
+{
+	printf("First name: ");
+  scanf("%s", stu->fn);
+
+	printf("Last name: ");
+  scanf("%s", stu->ln);
+
+	printf("Age: ");
+  scanf("%d", &(stu->age));
+
+	printf("Student id: ");
+  scanf("%d", &(stu->id));
+}
+
+int main()
+{
+	char answer;
+  int counter;
+	printf("Enter a student?(y/n) ");
+	scanf("%c", &answer);
+	counter = 0;
+	student students[100];
+
+	while (answer == 'y')
+	{
+		enterstud(&students[counter]);
+    counter++;
+    printf("Enter a student?(y/n) ");
+	  scanf(" %c", &answer);
+	}
+
+  for (int i = 0; i < counter; i++)
+  {
+    printstud(&students[i]);
+  }
+}
